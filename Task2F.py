ff0de81cbe233aa61962077a2f9307a738b74a65
jv436 <jv436@cam.ac.uk>
2022-03-01 17:55:45 +0000

    attempt at f

--- Task2F.py
+++ Task2F.py
@@ -15,9 +15,6 @@ def run():
         dates,levels = fetch_measure_levels(i[0].measure_id, dt=datetime.timedelta(days=dt))
         plot_water_level_with_fit(i[0], dates, levels, 4)
 
-
-    
-
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -22,17 +22,17 @@ def plot_water_levels(station, dates, levels):
 
 def plot_water_level_with_fit(station, dates, levels, p):
     datafetcher.fetch_latest_water_level_data(station)
-    update_water_levels(station)
     num_dates = matplotlib.dates.date2num(dates)
     coefficent= np.polyfit(num_dates - num_dates[0], levels, p)
     p_new = np.poly1d(coefficent)
-    x = polyfit(dates,levels,p_new)
+    date_range = np.linspace(num_dates[0], num_dates[-1], 60)
+    fit = polyfit(dates,levels,p_new)
+    plt.plot(date_range,station.typical_range[0],color="green",label="Low Typical Range")
+    plt.plot(date_range,station.typical_range[1],color="blue",label="High Typical Range")
     plt.plot(num_dates, levels,color="red",label="Actual Levels")
-    plt.plot(num_dates-x[1], levels, color="orange",label="Best fit line")
+    plt.plot(date_range, p_new(date_range - num_dates[0]), color="orange",label="Best fit line")
     plt.xlabel("numbered dates")
     plt.ylabel("water levels")
+    plt.legend()
     plt.title(station.name)
     plt.show()
-
-
-
