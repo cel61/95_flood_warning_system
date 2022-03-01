de3b4d1b2784e20fd81760154409c2b07e4c1090
cel61 <98656692+cel61@users.noreply.github.com>
2022-03-01 00:22:39 +0000

    commit on main

--- Task2B.py
+++ Task2B.py
@@ -1,1 +1,16 @@
 from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold
+
+def run():
+    stations = build_station_list()
+    tol = 0.8
+    result = stations_level_over_threshold(stations, tol)
+    for i in result:
+        print(i[0], i[1])
+
+
+
+if __name__ == "__main__":
+    print("*** Task 2A: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,7 +7,7 @@ def stations_level_over_threshold(stations, tol):
     dangerstations = []
     stations = build_station_list()
     for station in stations:
-        if station.relative_water_level > tol:
+        if float(station.relative_water_level) > tol:
             data = (station.name,station.relative_water_level)
             dangerstations.append(data)
     final = sorted_by_key(dangerstations,1,reverse=True)
