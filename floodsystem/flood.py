cdab620425b9f8291ecc7008aed28187077cecad
jv436 <jv436@cam.ac.uk>
2022-03-01 00:31:55 +0000

    commit on main

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -8,9 +8,8 @@ def stations_level_over_threshold(stations, tol):
     stations = build_station_list()
     for station in stations:
         rel_level= station.relative_water_level()
-        if rel_level is not None:
-            if rel_level > tol:
-                data = (station.name,station.relative_water_level)
-                dangerstations.append(data)
+        if rel_level > tol:
+            data = (station.name,station.relative_water_level)
+            dangerstations.append(data)
     final = sorted_by_key(dangerstations,1,reverse=True)
     return final
