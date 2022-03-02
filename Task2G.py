b723ce9ffaf529921582e36105b05ffff7c89323
jv436 <jv436@cam.ac.uk>
2022-03-02 14:15:05 +0000

    commit on main

--- Task2G.py
+++ Task2G.py
@@ -26,52 +26,60 @@ def run():
     for i in severe_old:
         if i[0].town == None:
             pass
-        if i[0].town in severe_towns:
+        elif i[0].town in severe_towns:
             pass
         else:
             severe.append(i[0].town)
     for j in hs:
         if j[0].town == None:
             pass
-        if j[0].town in severe_towns or high_towns:
+        elif j[0].town in severe_towns or high_towns:
             pass
         else:
             high.append(j[0].town)
     for k in ms:
         if k[0].town == None:
             pass
-        if k[0].town in severe_towns or high_towns or moderate_town:
+        elif k[0].town in severe_towns or high_towns or moderate_town:
             pass
         else:
             moderate.append(k[0].town)
     for l in ls:
         if l[0].town == None:
             pass
-        if l[0].town in severe_towns or high_towns or moderate_town or low_town:
+        elif l[0].town in severe_towns or high_towns or moderate_town or low_town:
             pass
         else:
             low.append(l[0].town)
+    severe.sort()
+    high.sort()
+    moderate.sort()
+    low.sort()
     print("SEVERE RISK")
     print("\n")
     for a in severe:
-        print("TOWN NAME: ",a)
+        if a != None:
+            print("TOWN NAME: ",a)
     print("\n")
     print("HIGH RISK")
     print("\n")
     for b in high:
-        print("TOWN NAME: ",b)
+        if b != None:
+            print("TOWN NAME: ",b)
     print("\n")
     print("MODERATE RISK")
     print("\n")
     for c in moderate:
-        print("TOWN NAME: ",c)
+        if c != None:
+            print("TOWN NAME: ",c)
     print("\n")
     print("LOW RISK")
     print("\n")
     for d in low:
-        print("TOWN NAME: ",d)
+        if d != None:
+            print("TOWN NAME: ",d)
     print("\n")
 
 if __name__ == "__main__":
-    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
